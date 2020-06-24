58173754b86a031d84ee0915100eab107704db70
Eugene <mihaylovskiyeugeny@gmail.com>
2020-06-24 16:21:09 +0300

    fixes

--- src/test/java/support/TestSettings.java
+++ src/test/java/support/TestSettings.java
@@ -3,9 +3,13 @@ package support;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TestSettings extends ChromeDriverBuilder {
+public class TestSettings {
 
-   public ChromeDriver chromeDriver = chromeDriver ();
+    public static ChromeDriver chromeDriver () {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver_83");
+        return new ChromeDriver();
+    }
+    public ChromeDriver chromeDriver = chromeDriver ();
     public WebDriverWait chromeWaiter = new WebDriverWait(chromeDriver, 5);
     public ScreenshotBuilder screenshotBuilder = new ScreenshotBuilder();
 
